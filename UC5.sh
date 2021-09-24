d20f77fb3aff72926e1b81066281f93be5d66fbc
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-24 23:06:08 +0530

    winnig Position 100

---
+++ UC5.sh
@@ -0,0 +1,56 @@
+using System;
+
+namespace UC5SnakeLadder
+{
+    public class SnakeLadder
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            int Max = 100;
+            int Player1 = 0;
+            while (position < 100)
+            {
+               
+                int dice = neww.RollDice();
+                Console.WriteLine("Dice value is : " + dice);
+               
+                 int option = neww.getOptions();
+                Console.WriteLine("Option is : " + option);
+
+               
+                switch (option)
+                {
+                    case 2:
+                        if (position + dice > 100)
+                        {
+                            position = position;
+                            break;
+                        }
+                        else
+                        {
+                            position = position + dice;
+                            Console.WriteLine("current position : " + position);
+                            break;
+                        }
+                    case 1:
+                        if (position <= 0)
+                        {
+                            position = 0;
+                            Console.WriteLine("current position : " + position);
+                        }
+                        else
+                            position = position - dice;
+                        Console.WriteLine("current position : " + position);
+                        break;
+                    default:
+                        position = position;
+                        Console.WriteLine("current position : " + position);
+                        break;
+                }
+            }
+            Player1 = position;
+            Console.WriteLine("Player Position is : " + Player1);
+
+        }
+    }
